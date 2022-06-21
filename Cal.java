71096ce924b8778b83b8b052a88f1793cbd30cb7
Pratik Jadhav <pratikjadhav9276@gmail.com>
2022-06-21 10:35:32 +0530

    Advanced Calculator

--- Cal.java
+++ Cal.java
@@ -13,7 +13,9 @@ public class Cal {
 		System.out.println("3.Substraction");
 		System.out.println("4.Divide");
 		System.out.println("5.Percentage");
-		System.out.println("6.Exit");
+		System.out.println("6.sin");
+		System.out.println("7.tan");
+		System.out.println("8.Exit");
 		choise=s.nextInt();
 		
 		switch(choise)
@@ -46,13 +48,32 @@ public class Cal {
 			  System.out.println("Divide: "+(num1/num2));
 			  break;
 			  
-		 
+		  case 5:
+			  System.out.println("Enter 4 subject mark :");
+			  int n1=s.nextInt();
+			  int n2=s.nextInt();
+			  int n3=s.nextInt();
+			  int n4=s.nextInt();
+			  System.out.println("Persentage : "+(((n1+n2+n3+n4)*100)/400));
+			  break;
 			  
 		  case 6:
+			  System.out.println("Enter number");
+			  double a=s.nextInt();
+			  System.out.println(" "+(Math.sin(a)));
+			  break;
+			  
+		  case 7:
+			  System.out.println("Enter number");
+			  double b=s.nextInt();
+			  System.out.println(" "+(Math.tan(b)));
+			  break;
+			  
+		  case 8:
 			  break;
 			  
 		}
-	}while(choise!=6);
+	}while(choise!=8);
   }
 }
 
